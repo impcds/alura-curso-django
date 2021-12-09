cd54c594d291ce3e8c810cdffd8897230e777d7e
Paulo César <paulocesarskt@gmail.com>
2021-12-09 14:50:17 -0300

    commit após concluir a segunda unidado do curso de django parte 3

--- alurareceitas/settings.py
+++ alurareceitas/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'receita',
     'pessoas',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'alurareceitas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'receita/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
